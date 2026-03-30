9dbb1bf3cd3003fac0ab75f7d89994a1bca18b02
workholics-shikha <workholics.shikha@gmail.com>
2026-03-30 18:27:04 +0530

    code updated

--- server/index.js
+++ server/index.js
@@ -1,19 +1,15 @@
 const express = require('express');
-
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const cors = require('cors'); // ✅ ADD
-
 const app = express();
-
-// ✅ ADD THIS
-app.use(cors({
-    origin: 'http://localhost:3000'
-}));
+const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
-app.use(express.json()); // important
+app.use(cors({
+  origin: '*'
+})); // important
 
 const userRoutes = require('./routes/userRoutes');
 const productRoutes = require('./routes/productRoutes');
@@ -23,21 +19,15 @@ app.use('/api/users', userRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/categories', categoryRoutes);
 
-const PORT = process.env.PORT || 5000;
-
 const startServer = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
         console.log('MongoDB connected');
 
-        // app.listen(PORT, () => {
-        //     console.log(`Server running on port ${PORT}`);
-        // });
-
         app.listen(PORT, () => {
             console.log(`Server running on port ${PORT}`);
         });
-        
+
     } catch (error) {
         console.error('Database connection failed:', error.message);
         process.exit(1);
