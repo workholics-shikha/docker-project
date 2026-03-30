2b3c9ad7794798ff11503c266cbead6636c00403
workholics-shikha <workholics.shikha@gmail.com>
2026-03-30 18:05:29 +0530

    server/index.js updated

--- server/index.js
+++ server/index.js
@@ -30,9 +30,10 @@ const startServer = async () => {
         await mongoose.connect(process.env.MONGO_URI);
         console.log('MongoDB connected');
 
-        app.listen(PORT, () => {
-            console.log(`Server running on port ${PORT}`);
-        });
+        // app.listen(PORT, () => {
+        //     console.log(`Server running on port ${PORT}`);
+        // });
+        app.listen(process.env.PORT || 5000)
     } catch (error) {
         console.error('Database connection failed:', error.message);
         process.exit(1);
